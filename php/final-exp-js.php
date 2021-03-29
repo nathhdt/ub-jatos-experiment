196933eb487c66ae43d5adda435f626b591b2102
Nathan Haudot <nathan.haudot@outlook.fr>
2021-03-30 01:06:08 +0200

    Finished subject retrieving

--- php/final-exp-js.php
+++ php/final-exp-js.php
@@ -5,8 +5,8 @@ require('sql.php');
 $email = $_GET['email'];
 
 // Montrer le détail des erreurs MySQL
-ini_set('error_reporting', -1);
-ini_set('display_errors', 1);
+//ini_set('error_reporting', -1);
+//ini_set('display_errors', 1);
 
 
 $stmt = $mysqli->prepare('SELECT `sujet` FROM `resultats` WHERE `email`= ?');
@@ -15,12 +15,25 @@ $stmt->bind_param("s", $email);
 
 $stmt->execute();
 
-$result = $stmt->get_result()->fetch_assoc());
+$stmt->bind_result($sujet);
 
-echo (int)$result; 
+/* Récupération des valeurs */
+$stmt->fetch();
+
+$stmt->close();
+
+// Echo du sujet
+if ($sujet == '')
+{
+	echo '0';
+}
+else
+{
+	echo $sujet;
+}
 
 // Echo les erreurs MySQL
-echo 'Error '.$mysqli->errno.': '.$mysqli->error.'<br/>';
+//echo 'Error '.$mysqli->errno.': '.$mysqli->error.'<br/>';
 
 // Ferme la connexion SQL
 $mysqli->close();
